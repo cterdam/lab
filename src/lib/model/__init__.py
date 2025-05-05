68a9d48b36bcbded70d4b373915e2bc73387bcea
Josh Lee <cterdam@google.com>
2025-05-04 18:02:40 -0700

    [model] move get lm to model

--- src/lib/model/__init__.py
+++ src/lib/model/__init__.py
@@ -1,1 +1,2 @@
+from src.lib.model.get_model import get_model as Model
 from src.lib.model.model_base import ModelBase
--- src/lib/model/txt/__init__.py
+++ src/lib/model/txt/__init__.py
@@ -1,2 +1,1 @@
-from src.lib.model.txt.get_lm import get_lm as LM
 from src.lib.model.txt.lm_base import LmBase

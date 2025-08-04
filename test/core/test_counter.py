2252f236d7d813bc67f7aa636703f16e1fb6a22c
Josh Lee <cterdam@google.com>
2025-08-04 06:46:36 +0000

    [test] more counter tests

--- test/core/test_counter.py
+++ test/core/test_counter.py
@@ -6,60 +6,60 @@ from multiprocessing import Pool
 
 import pytest
 
-from src.core import Logger
+from src import log
 
 
 def test_counter_no_concurrency():
-    logger_name = "test_counter_noconcur"
-    logger = Logger(logname=logger_name)
-    key = "count"
+    key = "test_counter_no_concurrency"
     num_incrs = 100
     val_per_incr = 2
     for _ in range(num_incrs):
-        logger.incr(key, val_per_incr)
-    assert logger.get(key) == num_incrs * val_per_incr
+        log.incr(key, val_per_incr)
+    assert log.get(key) == num_incrs * val_per_incr
 
 
 @pytest.mark.asyncio
 async def test_counter_async():
-    logger_name = "test_counter_async"
     num_attempts = 100
     num_incrs_per_attempt = 100
     val_per_incr = 2
-    key = "count"
+    key = "test_counter_async"
 
-    async def incr_count(logger: Logger):
-        for _ in range(num_incrs_per_attempt):
-            logger.incr(key, val_per_incr)
+    async def incr_count(num_incrs: int, val_per_incr: int):
+        for _ in range(num_incrs):
+            log.incr(key, val_per_incr)
             await asyncio.sleep(random.uniform(0, 0.001))
 
-    logger = Logger(logname=logger_name)
-    _ = await asyncio.gather(*(incr_count(logger) for _ in range(num_attempts)))
+    _ = await asyncio.gather(
+        *(incr_count(num_incrs_per_attempt, val_per_incr) for _ in range(num_attempts))
+    )
 
-    assert logger.get(key) == num_attempts * num_incrs_per_attempt * val_per_incr
+    assert log.get(key) == num_attempts * num_incrs_per_attempt * val_per_incr
 
 
 def test_counter_thread():
-    logger_name = "test_counter_thread"
     num_attempts = 100
     num_incrs_per_attempt = 100
     val_per_incr = 2
-    key = "count"
+    key = "test_counter_thread"
 
-    def incr_count(logger: Logger):
-        for _ in range(num_incrs_per_attempt):
-            logger.incr(key, val_per_incr)
+    def incr_count(num_incrs: int, val_per_incr: int):
+        for _ in range(num_incrs):
+            log.incr(key, val_per_incr)
             time.sleep(random.uniform(0, 0.001))
 
-    logger = Logger(logname=logger_name)
     with ThreadPoolExecutor(max_workers=num_attempts) as executor:
-        _ = list(executor.map(lambda _: incr_count(logger), range(num_attempts)))
+        _ = list(
+            executor.map(
+                lambda _: incr_count(num_incrs_per_attempt, val_per_incr),
+                range(num_attempts),
+            )
+        )
 
-    assert logger.get(key) == num_attempts * num_incrs_per_attempt * val_per_incr
+    assert log.get(key) == num_attempts * num_incrs_per_attempt * val_per_incr
 
 
 def process_incr_count(key: str, num_incrs_per_attempt: int, val_per_incr: int):
-    from src import log
 
     for _ in range(num_incrs_per_attempt):
         log.incr(key, val_per_incr)
@@ -70,7 +70,7 @@ def test_counter_process():
     num_attempts = 30
     num_incrs_per_attempt = 100
     val_per_incr = 2
-    key = "count"
+    key = "test_counter_process"
 
     with Pool(processes=num_attempts) as pool:
         _ = pool.starmap(
@@ -78,6 +78,35 @@ def test_counter_process():
             [(key, num_incrs_per_attempt, val_per_incr)] * num_attempts,
         )
 
-    from src import log
-
     assert log.get(key) == num_attempts * num_incrs_per_attempt * val_per_incr
+
+
+def test_counter_negative():
+    key = "test_counter_negative"
+    init_val = 0
+    num_incrs = 100
+    val_per_incr = -1
+
+    assert val_per_incr < 0
+
+    log.set(key, init_val)
+    for _ in range(num_incrs):
+        log.incr(key, val_per_incr)
+
+    assert log.get(key) == num_incrs * val_per_incr
+
+
+def test_counter_absent_key():
+    key = "test_counter_absent_key"
+    incr_val = 10
+    assert log.get(key) is None
+
+    log.incr(key, incr_val)
+    assert log.get(key) == incr_val
+
+
+def test_counter_set_val():
+    key = "test_counter_set_val"
+    val = 10
+    log.set(key, val)
+    assert log.get(key) == val
